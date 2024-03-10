034d39a2a8dd5f0a494dfeaefdc2307986458c0e
Guolin.Wan <wanguolin.gmail.com>
2024-03-10 13:36:12 +0800

    redirect notion public site

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ const port = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get('/notion', async (req, res) => {
+    const notionUrl = 'https://gentiles.notion.site/5a8cbff1c86042599e62f47f4973c503?pvs=4';
+    res.redirect(notionUrl);
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  
